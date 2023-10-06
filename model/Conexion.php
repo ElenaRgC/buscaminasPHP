729553784d2bc54071c5d5364a3338c9aacb75f3
Elena Rodríguez <95099947+ElenaRgC@users.noreply.github.com>
2023-10-06 18:23:37 +0200

    Creadas funciones Conectar() y Desconectar()

--- model/Conexion.php
+++ model/Conexion.php
@@ -1,3 +1,29 @@
 <?php
 
 require_once '../Constantes.php';
+
+class Conexion
+{
+    public static $conexion;
+
+    public static function conectar()
+    {
+        try {
+            self::$conexion = new mysqli(
+                Constantes::$URL,
+                Constantes::$USER,
+                Constantes::$PASSWORD,
+                Constantes::$DATABASE_NAME
+            );
+
+            return self::$conexion;
+        } catch (Exception $e) {
+            return 0;
+        }
+    }
+
+    public static function desconectar()
+    {
+        self::$conexion->close();
+    }
+}
