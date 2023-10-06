f5c71dcfc9dad480e17c7c92faceb5a1c777b625
Elena Rodríguez <95099947+ElenaRgC@users.noreply.github.com>
2023-10-06 18:55:40 +0200

    Creado SELECT de Jugador

--- model/Conexion.php
+++ model/Conexion.php
@@ -1,6 +1,7 @@
 <?php
 
-require_once '../Constantes.php';
+require_once __DIR__.'\..\Constantes.php';
+require_once __DIR__.'\..\Factoria.php';
 
 class Conexion
 {
@@ -25,5 +26,32 @@ class Conexion
     public static function desconectar()
     {
         self::$conexion->close();
+    }
+
+// JUGADOR -------------------------------------
+
+    public static function getJugadores() {
+        self::$conexion = self::conectar();
+
+        $query = 'SELECT * FROM jugador';
+
+        $stmt = self::$conexion->prepare($query);
+
+        try {
+            $stmt->execute();
+            $resultados = $stmt->get_result();
+
+            while ($fila = $resultados->fetch_array()) {
+                $p = Factoria::crearJugador($fila[0], $fila[1], $fila[2], $fila[3], $fila[4]);
+            }
+
+            $resultados->free_result();
+
+            return $p;
+        } catch (Exception $e) {
+            return 0;
+        } finally {
+            self::desconectar();
+        }
     }
 }
