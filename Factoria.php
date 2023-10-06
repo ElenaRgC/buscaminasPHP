8540f4f5b6dd56a12292bee27aa040c9af3f8594
Elena Rodríguez <95099947+ElenaRgC@users.noreply.github.com>
2023-10-06 18:28:46 +0200

    crearJugador() agregado a Factoria

--- Factoria.php
+++ Factoria.php
@@ -2,3 +2,11 @@
 
 require_once 'model/Jugador.php';
 require_once 'model/Partida.php';
+
+class Factoria {
+
+    public static function crearJugador($id, $nombre, $email, $partidasJugadas, $partidasGanadas) {
+        return new Jugador($id, $nombre, $email, $partidasJugadas, $partidasGanadas);
+    }
+
+}
