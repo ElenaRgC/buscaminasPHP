3852a1fdd6f523d55ca76739e57168224e4229a7
Elena Rodriguez <elenarodriguezcalderon@gmail.com>
2023-10-10 12:41:49 +0200

    Añadida condición esAdmin a getJugadores

--- controllers/Controlador.php
+++ controllers/Controlador.php
@@ -5,8 +5,10 @@ require_once __DIR__.'..\model\Factoria.php';
 
 class Controlador
 {
-    public static function login($jugador, $datosRecibidos)
+    public static function login($datosRecibidos)
     {
+        $jugador = Conexion::getJugadorFromEmail($datosRecibidos['email']);
+
         if ($jugador instanceof Jugador) {
             if ($jugador->getEmail() == $datosRecibidos['email'] && $jugador->getPass() == $datosRecibidos['pass']) {
                 $esAdmin = false;
@@ -29,18 +31,22 @@ class Controlador
     {
         $jugadores = Conexion::getJugadores();
 
-        if ($jugadores[0] instanceof Jugador) {
-            $cod = 200;
-            $mes = 'OK';
+        $esAdmin = Controlador::login($datosRecibidos);
 
-            return json_encode(['Codigo' => $cod, 'Mensaje' => $mes]);
+        if ($esAdmin[0] && $esAdmin[1]) {
+            // [0] login correcto [1] es Administrador
 
-            return json_encode(['Codigo' => $cod, 'Mensaje' => $mes, 'Jugadores' => $jugadores]);
-        } else {
-            $cod = 500;
-            $mes = 'Error';
+            if ($jugadores[0] instanceof Jugador) {
+                $cod = 200;
+                $mes = 'OK';
 
-            return json_encode(['Codigo' => $cod, 'Mensaje' => $mes]);
+                return json_encode(['Codigo' => $cod, 'Mensaje' => $mes, 'Jugadores' => $jugadores]);
+            } else {
+                $cod = 500;
+                $mes = 'Error';
+
+                return json_encode(['Codigo' => $cod, 'Mensaje' => $mes]);
+            }
         }
     }
 }
