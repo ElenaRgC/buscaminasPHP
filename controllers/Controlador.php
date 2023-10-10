4dfb9f2d922073c36894e500f6ea1b94dd433b01
Elena Rodriguez <elenarodriguezcalderon@gmail.com>
2023-10-10 09:29:22 +0200

    Creada funcion getJugadores()

--- controllers/Controlador.php
+++ controllers/Controlador.php
@@ -24,4 +24,23 @@ class Controlador
             }
         }
     }
+
+    public static function getJugadores()
+    {
+        $jugadores = Conexion::getJugadores();
+
+        if ($jugadores[0] instanceof Jugador) {
+            $cod = 200;
+            $mes = 'OK';
+
+            return json_encode(['Codigo' => $cod, 'Mensaje' => $mes]);
+
+            return json_encode(['Codigo' => $cod, 'Mensaje' => $mes, 'Jugadores' => $jugadores]);
+        } else {
+            $cod = 500;
+            $mes = 'Error';
+
+            return json_encode(['Codigo' => $cod, 'Mensaje' => $mes]);
+        }
+    }
 }
