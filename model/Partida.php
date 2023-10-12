5cde122e9a782b6e98733adf2879a05ff0f5a651
Elena Rodriguez <elenarodriguezcalderon@gmail.com>
2023-10-12 11:21:38 +0200

    Añadido toString() para pruebas

--- model/Partida.php
+++ model/Partida.php
@@ -1,57 +1,78 @@
 <?php
-class Partida {
+
+class Partida
+{
     public $id;
     public $idJugador;
     public $tableroSolucion;
     public $tableroJugador;
     public $fin;
 
-	public function __constructor($id, $idJugador, $tableroSolucion, $tableroJugador, $fin) {
+    public function __construct($id, $idJugador, $tableroSolucion, $tableroJugador, $fin)
+    {
+        $this->id = $id;
+        $this->idJugador = $idJugador;
+        $this->tableroSolucion = $tableroSolucion;
+        $this->tableroJugador = $tableroJugador;
+        $this->fin = $fin;
+    }
 
-		$this->id = $id;
-		$this->idJugador = $idJugador;
-		$this->tableroSolucion = $tableroSolucion;
-		$this->tableroJugador = $tableroJugador;
-		$this->fin = $fin;
-	}
+    public function getId()
+    {
+        return $this->id;
+    }
 
-	public function getId() {
-		return $this->id;
-	}
+    public function setId($value)
+    {
+        $this->id = $value;
+    }
 
-	public function setId($value) {
-		$this->id = $value;
-	}
+    public function getIdJugador()
+    {
+        return $this->idJugador;
+    }
 
-	public function getIdJugador() {
-		return $this->idJugador;
-	}
+    public function setIdJugador($value)
+    {
+        $this->idJugador = $value;
+    }
 
-	public function setIdJugador($value) {
-		$this->idJugador = $value;
-	}
+    public function getTableroSolucion()
+    {
+        return $this->tableroSolucion;
+    }
 
-	public function getTableroSolucion() {
-		return $this->tableroSolucion;
-	}
+    public function setTableroSolucion($value)
+    {
+        $this->tableroSolucion = $value;
+    }
 
-	public function setTableroSolucion($value) {
-		$this->tableroSolucion = $value;
-	}
+    public function getTableroJugador()
+    {
+        return $this->tableroJugador;
+    }
 
-	public function getTableroJugador() {
-		return $this->tableroJugador;
-	}
+    public function setTableroJugador($value)
+    {
+        $this->tableroJugador = $value;
+    }
 
-	public function setTableroJugador($value) {
-		$this->tableroJugador = $value;
-	}
+    public function getFin()
+    {
+        return $this->fin;
+    }
 
-	public function getFin() {
-		return $this->fin;
-	}
+    public function setFin($value)
+    {
+        $this->fin = $value;
+    }
 
-	public function setFin($value) {
-		$this->fin = $value;
-	}
+    public function __toString()
+    {
+        return 'ID: '.$this->id."\n".
+               'ID del Jugador: '.$this->idJugador."\n".
+               'Tablero Solución: '.$this->tableroSolucion."\n".
+               'Tablero Jugador: '.$this->tableroJugador."\n".
+               'Fin: '.$this->fin;
+    }
 }
