5e43dc8c6cd52b72d28b19df0d0c40dd8e343e0d
Elena Rodriguez <elenarodriguezcalderon@gmail.com>
2023-10-12 11:22:16 +0200

    Añadida lógica creación partidas

--- auxiliar/Factoria.php
+++ auxiliar/Factoria.php
@@ -14,4 +14,44 @@ class Factoria
     {
         return new Partida($id, $idJugador, $tableroSolucion, $tableroJugador, $fin);
     }
+
+    public static function crearPartidaNueva($idJugador, $longitud, $bombas)
+    {
+        $tableroSolucion = Factoria::crearTableroSolucion($longitud, $bombas);
+        $tableroJugador = Factoria::crearTableroJugador($longitud);
+
+        return Factoria::crearPartida(0, $idJugador, $tableroSolucion, $tableroJugador, 0);
+    }
+
+    private static function crearTableroSolucion($longitud, $bombas)
+    {
+        $tablero = array_fill(1, $longitud, 0);
+
+        while ($bombas > 0) {
+            $rand = rand(1, $longitud);
+
+            if ($tablero[$rand] != '*') {
+                $tablero[$rand] = '*';
+                --$bombas;
+            }
+        }
+
+        for ($i = 1; $i <= $longitud; ++$i) {
+            if ($tablero[$i] != '*') {
+                if ($i > 1 && $tablero[$i - 1] == '*') {
+                    ++$tablero[$i];
+                }
+                if ($i < $longitud && $tablero[$i + 1] == '*') {
+                    ++$tablero[$i];
+                }
+            }
+        }
+
+        return implode('', $tablero);
+    }
+
+    private static function crearTableroJugador($longitud)
+    {
+        return implode('', array_fill(1, $longitud, '-'));
+    }
 }
